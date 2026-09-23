0e0de520d05bd18a31e6ffc98f61fdd753f85c40
gopal974925 <gbhunia819@gmail.com>
2026-09-23 23:52:27 +0530

    feat: add response-time middleware for request/response timing metrics

--- job/src/index.ts
+++ job/src/index.ts
@@ -3,10 +3,26 @@ import dotenv from"dotenv"
 import {sql} from "./utils/db.js"
 import { connnectKafka } from "./utils/producer.js";
 import client from "prom-client";
+import responsetime from "response-time"
 dotenv.config();
 
 const collectDefaultmetrix=client.collectDefaultMetrics;
 collectDefaultmetrix({register:client.register})
+
+const reqResTime=new client.Histogram({
+  name:"http_express_req_res_time",
+  help:"This tells how much time is taken by req and res",
+  labelNames:["method","route","status_code"],
+  buckets:[1,50,100,200,400,500,80,1000,2000,3000]
+})
+
+app.use(responsetime((req, res, time) => {
+    reqResTime.labels({
+      method:req.method,
+      route:req.url,
+      status_code:res.statusCode,
+    }).observe(time);
+}))
 
 app.get("/metrics",async (req,res)=>{
   res.setHeader("content-Type",client.register.contentType);
